9de74c5fafda16cb87dddc81a226a5f5bc04281b
edalzell <erin@thedalzells.org>
2021-01-27 11:28:15 -0800

    don’t load if not configured

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,19 +1,20 @@
-var script = document.createElement("script");
-script.src = 'https://chat-assets.frontapp.com/v1/chat.bundle.js';
+let frontConfig = Statamic.$config.get('front');
 
-// add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+if (frontConfig && frontConfig.chatId) {
+    var script = document.createElement("script");
+    script.src = 'https://chat-assets.frontapp.com/v1/chat.bundle.js';
 
-document.body.appendChild(script);
-script.addEventListener('load', () => {
-    let frontConfig = Statamic.$config.get('front');
-
-    window.FrontChat(
-        'init',
-        {
-            chatId: frontConfig.chatId,
-            email: frontConfig.email,
-            userHash: frontConfig.hash,
-            useDefaultLauncher: true
-        }
-    );
-});
+    // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+    document.body.appendChild(script);
+    script.addEventListener('load', () => {
+        window.FrontChat(
+            'init',
+            {
+                chatId: frontConfig.chatId,
+                email: frontConfig.email,
+                userHash: frontConfig.hash,
+                useDefaultLauncher: true
+            }
+        );
+    });
+}
