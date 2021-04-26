b87aefaeebc46ffb81c42c5d96dfdf62145a0987
edalzell <erin@thedalzells.org>
2021-04-26 12:23:28 -0700

    gotta wait until statamic is booted to run the js

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,20 +1,22 @@
-let frontConfig = Statamic.$config.get('front');
+Statamic.booted(() => {
+    let frontConfig = Statamic.$config.get('front');
 
-if (frontConfig && frontConfig.chatId) {
-    var script = document.createElement("script");
-    script.src = 'https://chat-assets.frontapp.com/v1/chat.bundle.js';
+    if (frontConfig && frontConfig.chatId) {
+        var script = document.createElement("script");
+        script.src = 'https://chat-assets.frontapp.com/v1/chat.bundle.js';
 
-    // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
-    document.body.appendChild(script);
-    script.addEventListener('load', () => {
-        window.FrontChat(
-            'init',
-            {
-                chatId: frontConfig.chatId,
-                email: frontConfig.email,
-                userHash: frontConfig.hash,
-                useDefaultLauncher: true
-            }
-        );
-    });
-}
+        // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+        document.body.appendChild(script);
+        script.addEventListener('load', () => {
+            window.FrontChat(
+                'init',
+                {
+                    chatId: frontConfig.chatId,
+                    email: frontConfig.email,
+                    userHash: frontConfig.hash,
+                    useDefaultLauncher: true
+                }
+            );
+        });
+    }
+});
