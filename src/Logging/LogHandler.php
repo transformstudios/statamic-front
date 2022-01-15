34e1d6ea9371dbfc3759b93d229f5ad695999ea1
edalzell <erin@silentz.co>
2022-01-14 16:44:20 -0800

    refactor
    https://github.com/transformstudios/zakat-v3/issues/232

--- src/Logging/LogHandler.php
+++ src/Logging/LogHandler.php
@@ -2,6 +2,7 @@
 
 namespace TransformStudios\Front\Logging;
 
+use Illuminate\Support\Collection;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger as Monolog;
 use Statamic\Support\Arr;
@@ -19,22 +20,30 @@ class LogHandler extends AbstractProcessingHandler
     {
         $conversation = config('front.logging.conversation_id');
 
-        $data = $this->convertErrorToFrontMessage(Arr::get($record, 'context.exception'));
         front()
-            ->post("/conversations/$conversation/comments", $data)
-            ->throw();
+            ->post(
+                "/conversations/$conversation/comments",
+                $this->convertErrorToFrontMessage(Arr::get($record, 'context.exception'))
+            )->throw();
     }
 
     private function convertErrorToFrontMessage(Throwable $error): array
     {
-        $body = '**'.$error->getMessage().'**'.PHP_EOL;
-        $body .= '* '.$error->getFile().' ('.$error->getLine().')'.PHP_EOL;
+        return ['body' => $this->formatErrorLines($error)->implode(PHP_EOL)];
+    }
 
-        $body .= collect($error->getTrace())
+    private function formatErrorLines(Throwable $error): Collection
+    {
+        return collect([
+                '**'.$error->getMessage().'**',
+                '* '.$error->getFile().' ('.$error->getLine().')',
+            ])->merge($this->formatStackTrace($error));
+    }
+
+    private function formatStackTrace(Throwable $error): Collection
+    {
+        return collect($error->getTrace())
             ->take(10)
-            ->map(fn (array $traceItem) => '* '.$traceItem['file'].' ('.$traceItem['line'].')')
-            ->implode(PHP_EOL);
-
-        return ['body' => $body];
+            ->map(fn (array $traceItem) => '* '.$traceItem['file'].' ('.$traceItem['line'].')');
     }
 }
