c85c47ad752ee43cf9d6011a1f24ede3dff8a749
edalzell <erin@silentz.co>
2022-01-14 16:17:38 -0800

    helper
    https://github.com/transformstudios/zakat-v3/issues/232

---
+++ src/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Support\Facades\Http;
+
+function front(): PendingRequest
+{
+    return Http::withToken(config('front.api_token'))->baseUrl(
+        'https://api2.frontapp.com'
+    );
+}
