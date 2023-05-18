0f0fbee1845d87995e830ee2678ebc8cafa9a7a2
Erin Dalzell <erin@silentz.co>
2023-05-18 10:26:34 -0700

    Statamic v4 (#10)

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -15,12 +15,13 @@ use TransformStudios\Front\Notifications\Channel;
 
 class ServiceProvider extends AddonServiceProvider
 {
-    protected $scripts = [
-        __DIR__.'/../dist/js/cp.js',
-    ];
-
-    protected $stylesheets = [
-        __DIR__.'/../resources/css/cp.css',
+    protected $vite = [
+        'input' => [
+            'resources/css/cp.css',
+            'resources/js/cp.js',
+        ],
+        'publicDirectory' => 'dist',
+        'hotFile' => __DIR__.'/../dist/hot',
     ];
 
     public function bootAddon()
