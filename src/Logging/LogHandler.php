d60140f3b956b854427b23685f1a493135b93b03
Erin Dalzell <erin@silentz.co>
2023-06-09 13:55:43 -0700

    Make compatible w/ latest version (#11)

--- src/Logging/LogHandler.php
+++ src/Logging/LogHandler.php
@@ -5,6 +5,7 @@ namespace TransformStudios\Front\Logging;
 use Illuminate\Support\Collection;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger as Monolog;
+use Monolog\LogRecord;
 use Statamic\Support\Arr;
 use Throwable;
 
@@ -16,18 +17,18 @@ class LogHandler extends AbstractProcessingHandler
         parent::__construct(Monolog::toMonologLevel($channelConfig['level'] ?? Monolog::DEBUG));
     }
 
-    public function write(array $record): void
+    public function write(LogRecord $record): void
     {
         if (! $conversation = config('front.logging.conversation_id')) {
             return;
         }
 
-        if (! $error = Arr::get($record, 'context.exception')) {
+        if (! Arr::get($record->context, 'exception')) {
             $errors = collect(
                 [
                     'Request URL: '.request()->fullUrl(),
                     'Request data: '.json_encode(request()->input()),
-                    'Error: '.json_encode($record),
+                    'Error: '.json_encode($record->toArray()),
                 ]
             );
 
@@ -43,7 +44,7 @@ class LogHandler extends AbstractProcessingHandler
         front()
             ->post(
                 "/conversations/$conversation/comments",
-                $this->convertErrorToFrontMessage(Arr::get($record, 'context.exception'))
+                $this->convertErrorToFrontMessage(Arr::get($record->context, 'exception'))
             )->throw();
     }
 
